072102a7fcd895933f03848c737a6d26e1368f96
twodam <twodam@protonmail.com>
2023-10-25 16:54:34 +0800

    feat: init package and add types from nance-ts

---
+++ src/types/apiModel.ts
@@ -0,0 +1,82 @@
+import { DateEvent, GovernanceCycleForm, NanceConfig, Proposal } from './common';
+import { SQLPayout, DialogHandlerMessageIds } from './doltSchema';
+
+interface APIResponse<T> {
+  success: boolean;
+  error?: string;
+  data: T;
+}
+
+export type SpaceInfo = {
+  name: string;
+  currentCycle: number;
+  currentEvent: DateEvent;
+  spaceOwners: string[];
+  snapshotSpace: string;
+  juiceboxProjectId: string;
+  transactorAddress?: {
+    type: 'safe' | 'governor';
+    network: string;
+    address: string;
+  }
+  dolthubLink: string;
+};
+
+export type SpaceInfoExtended = SpaceInfo & {
+  currentDay: number;
+  cycleTriggerTime: string;
+  dialog: DialogHandlerMessageIds;
+  config: NanceConfig;
+};
+
+type ProposalInfo = {
+  snapshotSpace: string;
+  proposalIdPrefix: string;
+  minTokenPassingAmount: number;
+};
+
+export type ProposalsPacket = { proposalInfo: ProposalInfo; proposals: Proposal[]; hasMore: boolean; };
+
+export type ProposalsQueryResponse = APIResponse<ProposalsPacket>;
+
+export type ProposalMarkdownResponse = APIResponse<Proposal>;
+
+export type PayoutsQueryResponse = APIResponse<SQLPayout[]>;
+
+export type SpaceInfoResponse = APIResponse<SpaceInfo>;
+
+export type APIErrorResponse = APIResponse<undefined>;
+
+interface BaseRequest {
+  space: string;
+}
+
+export interface SpaceProposalRequest extends BaseRequest {
+  cycle: number | undefined;
+}
+
+export type SpaceInfoRequest = BaseRequest;
+
+export interface ProposalUploadRequest extends BaseRequest {
+  proposal: Proposal;
+}
+
+export interface FetchReconfigureRequest extends BaseRequest {
+  version: string;
+  address: string;
+  datetime: string;
+  network: string;
+}
+
+export interface ConfigSpaceRequest extends BaseRequest {
+  config: NanceConfig;
+  owners: string[];
+  cycleTriggerTime: string;
+  cycleStageLengths: number[];
+  governanceCycleForm: GovernanceCycleForm
+  dryrun?: boolean;
+}
+
+export interface EditPayoutsRequest extends BaseRequest {
+  payouts: SQLPayout[];
+}
