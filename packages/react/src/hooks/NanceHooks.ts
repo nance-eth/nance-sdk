53eee5fd7dcc3a1e5fd701fd1c6e7b13102fc9ba
twodam <twodam@protonmail.com>
2024-05-09 16:11:07 +0800

    fix: add nextProposalId to placeholder proposalPacket; remove reconfigure endpoint

--- packages/react/src/hooks/NanceHooks.ts
+++ packages/react/src/hooks/NanceHooks.ts
@@ -1,31 +1,31 @@
-import useSWR, { Fetcher } from 'swr';
-import useSWRMutation from 'swr/mutation';
-import useSWRInfinite from 'swr/infinite';
+import useSWR, { Fetcher } from "swr";
+import useSWRMutation from "swr/mutation";
+import useSWRInfinite from "swr/infinite";
 import {
   APIResponse,
   ProposalsRequest,
   SpaceInfoRequest,
   ProposalRequest,
   ProposalUploadRequest,
-  FetchReconfigureRequest,
   SpaceInfo,
   Proposal,
-  FetchReconfigureData,
   ProposalUploadPayload,
   ProposalsPacket,
   SQLPayout,
   ConfigSpacePayload,
   CreateFormValues,
 } from "@nance/nance-sdk";
-import { useContext } from 'react';
-import { NanceContext } from '../contexts/NanceContext';
+import { useContext } from "react";
+import { NanceContext } from "../contexts/NanceContext";
 
 function jsonFetcher(): Fetcher<APIResponse<any>, string> {
   return async (url) => {
     const res = await fetch(url);
     const json = await res.json();
-    if (json?.success === 'false') {
-      throw new Error(`An error occurred while fetching the data: ${json?.error}`);
+    if (json?.success === "false") {
+      throw new Error(
+        `An error occurred while fetching the data: ${json?.error}`,
+      );
     }
     return json;
   };
@@ -35,32 +35,42 @@ export function useAllSpaceInfo(shouldFetch: boolean = true) {
   const { apiUrl } = useContext(NanceContext);
   return useSWR<APIResponse<SpaceInfo[]>>(
     shouldFetch ? `${apiUrl}/ish/all` : null,
-    jsonFetcher()
+    jsonFetcher(),
   );
 }
 
-export function useSpaceInfo(args: SpaceInfoRequest, shouldFetch: boolean = true) {
+export function useSpaceInfo(
+  args: SpaceInfoRequest,
+  shouldFetch: boolean = true,
+) {
   const { apiUrl } = useContext(NanceContext);
   return useSWR<APIResponse<SpaceInfo>, string>(
     shouldFetch ? `${apiUrl}/${args.space}` : null,
-    jsonFetcher()
+    jsonFetcher(),
   );
 }
 
-export function useCurrentPayouts(space: string, cycle: string | undefined, shouldFetch: boolean = true) {
+export function useCurrentPayouts(
+  space: string,
+  cycle: string | undefined,
+  shouldFetch: boolean = true,
+) {
   const { apiUrl } = useContext(NanceContext);
   const urlParams = new URLSearchParams();
   if (cycle) {
-    urlParams.set('cycle', cycle);
+    urlParams.set("cycle", cycle);
   }
 
   return useSWR<APIResponse<SQLPayout[]>, string>(
     shouldFetch ? `${apiUrl}/${space}/payouts?` + urlParams.toString() : null,
-    jsonFetcher()
+    jsonFetcher(),
   );
 }
 
-export function usePrivateProposals(space: string, shouldFetch: boolean = true) {
+export function usePrivateProposals(
+  space: string,
+  shouldFetch: boolean = true,
+) {
   const { apiUrl } = useContext(NanceContext);
   return useSWR<APIResponse<Proposal[]>, string>(
     shouldFetch ? `${apiUrl}/${space}/privateProposals` : null,
@@ -68,102 +78,126 @@ export function usePrivateProposals(space: string, shouldFetch: boolean = true) 
   );
 }
 
-export function useProposals(args: ProposalsRequest, shouldFetch: boolean = true) {
+export function useProposals(
+  args: ProposalsRequest,
+  shouldFetch: boolean = true,
+) {
   const { apiUrl } = useContext(NanceContext);
   const urlParams = new URLSearchParams();
   if (args.cycle) {
-    urlParams.set('cycle', args.cycle);
+    urlParams.set("cycle", args.cycle);
   }
   if (args.keyword) {
-    urlParams.set('keyword', args.keyword);
+    urlParams.set("keyword", args.keyword);
   }
   if (args.limit) {
-    urlParams.set('limit', args.limit.toString());
+    urlParams.set("limit", args.limit.toString());
   }
   if (args.page) {
-    urlParams.set('page', args.page.toString());
+    urlParams.set("page", args.page.toString());
   }
 
   return useSWR<APIResponse<ProposalsPacket>, string>(
-    shouldFetch ? `${apiUrl}/${args.space}/proposals?` + urlParams.toString() : null,
+    shouldFetch
+      ? `${apiUrl}/${args.space}/proposals?` + urlParams.toString()
+      : null,
     jsonFetcher(),
   );
 }
 
-export function useProposalsInfinite(args: ProposalsRequest, shouldFetch: boolean = true) {
+export function useProposalsInfinite(
+  args: ProposalsRequest,
+  shouldFetch: boolean = true,
+) {
   const { apiUrl } = useContext(NanceContext);
   const urlParams = new URLSearchParams();
   if (args.cycle) {
-    urlParams.set('cycle', args.cycle);
+    urlParams.set("cycle", args.cycle);
   }
   if (args.keyword) {
-    urlParams.set('keyword', args.keyword);
+    urlParams.set("keyword", args.keyword);
   }
   if (args.limit) {
-    urlParams.set('limit', args.limit.toString());
+    urlParams.set("limit", args.limit.toString());
   }
   if (args.page) {
-    urlParams.set('page', args.page.toString());
+    urlParams.set("page", args.page.toString());
   }
 
-  const getKey = (pageIndex: number, previousPageData: APIResponse<ProposalsPacket>) => {
-    if (!shouldFetch || (previousPageData && !previousPageData.data.hasMore)) return null; // reached the end
-    urlParams.set('page', (pageIndex + 1).toString());
-    return `${apiUrl}/${args.space}/proposals?` + urlParams.toString();                    // SWR key
+  const getKey = (
+    pageIndex: number,
+    previousPageData: APIResponse<ProposalsPacket>,
+  ) => {
+    if (!shouldFetch || (previousPageData && !previousPageData.data.hasMore))
+      return null; // reached the end
+    urlParams.set("page", (pageIndex + 1).toString());
+    return `${apiUrl}/${args.space}/proposals?` + urlParams.toString(); // SWR key
   };
 
-  return useSWRInfinite<APIResponse<ProposalsPacket>, string>(getKey, jsonFetcher());
+  return useSWRInfinite<APIResponse<ProposalsPacket>, string>(
+    getKey,
+    jsonFetcher(),
+  );
 }
 
-export function useProposal(args: ProposalRequest, shouldFetch: boolean = true) {
+export function useProposal(
+  args: ProposalRequest,
+  shouldFetch: boolean = true,
+) {
   const { apiUrl } = useContext(NanceContext);
   return useSWR<APIResponse<Proposal>, string>(
-    shouldFetch ? `${apiUrl}/${args.space}/proposal/${args.hash}` : null,
+    shouldFetch ? `${apiUrl}/${args.space}/proposal/${args.uuid}` : null,
     jsonFetcher(),
   );
 }
 
-export function useReconfigureRequest(args: FetchReconfigureRequest, shouldFetch: boolean = true) {
-  const { apiUrl } = useContext(NanceContext);
-  return useSWR<APIResponse<FetchReconfigureData>, string>(
-    shouldFetch ? `${apiUrl}/${args.space}/reconfigure?version=${args.version}&address=${args.address}&datetime=${args.datetime}&network=${args.network}` : null,
-    jsonFetcher(),
-  );
-}
-
-async function uploader(url: RequestInfo | URL, { arg }: { arg: ProposalUploadRequest }) {
+async function uploader(
+  url: RequestInfo | URL,
+  { arg }: { arg: ProposalUploadRequest },
+) {
   const res = await fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(arg),
   });
   const json: APIResponse<ProposalUploadPayload> = await res.json();
   if (json.success === false) {
-    throw new Error(`An error occurred while uploading the data: ${json?.error}`);
+    throw new Error(
+      `An error occurred while uploading the data: ${json?.error}`,
+    );
   }
 
   return json;
 }
 
-async function creator(url: RequestInfo | URL, { arg }: { arg: CreateFormValues }) {
+async function creator(
+  url: RequestInfo | URL,
+  { arg }: { arg: CreateFormValues },
+) {
   const res = await fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(arg)
+    body: JSON.stringify(arg),
   });
   const json: APIResponse<ConfigSpacePayload> = await res.json();
   if (json.success === false) {
-    throw new Error(`An error occurred while uploading the data: ${json?.error}`);
+    throw new Error(
+      `An error occurred while uploading the data: ${json?.error}`,
+    );
   }
 
   return json;
 }
 
-export function useProposalUpload(space: string, proposalId: string | undefined, shouldFetch: boolean = true) {
+export function useProposalUpload(
+  space: string,
+  proposalId: string | undefined,
+  shouldFetch: boolean = true,
+) {
   const { apiUrl } = useContext(NanceContext);
   let url = `${apiUrl}/${space}/proposals`;
   let fetcher = uploader;
@@ -171,33 +205,36 @@ export function useProposalUpload(space: string, proposalId: string | undefined,
     url = `${apiUrl}/${space}/proposal/${proposalId}`;
     fetcher = editor;
   }
-  return useSWRMutation(
-    shouldFetch ? url : null,
-    fetcher,
-  );
+  return useSWRMutation(shouldFetch ? url : null, fetcher);
 }
 
-export function useProposalDelete(space: string, uuid: string | undefined, shouldFetch: boolean = true) {
+export function useProposalDelete(
+  space: string,
+  uuid: string | undefined,
+  shouldFetch: boolean = true,
+) {
   const { apiUrl } = useContext(NanceContext);
   let url = `${apiUrl}/${space}/proposal/${uuid}`;
   let fetcher = deleter;
-  return useSWRMutation(
-    shouldFetch ? url : null,
-    fetcher,
-  );
+  return useSWRMutation(shouldFetch ? url : null, fetcher);
 }
 
-async function editor(url: RequestInfo | URL, { arg }: { arg: ProposalUploadRequest }) {
+async function editor(
+  url: RequestInfo | URL,
+  { arg }: { arg: ProposalUploadRequest },
+) {
   const res = await fetch(url, {
-    method: 'PUT',
+    method: "PUT",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(arg)
+    body: JSON.stringify(arg),
   });
   const json: APIResponse<ProposalUploadPayload> = await res.json();
   if (json.success === false) {
-    throw new Error(`An error occurred while uploading the data: ${json?.error}`);
+    throw new Error(
+      `An error occurred while uploading the data: ${json?.error}`,
+    );
   }
 
   return json;
@@ -205,14 +242,16 @@ async function editor(url: RequestInfo | URL, { arg }: { arg: ProposalUploadRequ
 
 async function deleter(url: RequestInfo | URL) {
   const res = await fetch(url, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
   });
   const json: APIResponse<ProposalUploadPayload> = await res.json();
   if (json.success === false) {
-    throw new Error(`An error occurred while deleting this proposal: ${json?.error}`);
+    throw new Error(
+      `An error occurred while deleting this proposal: ${json?.error}`,
+    );
   }
 
   return json;
@@ -222,13 +261,15 @@ export function useCreateSpace(shouldFetch: boolean = true) {
   const { apiUrl } = useContext(NanceContext);
   const url = `${apiUrl}/ish/config`;
   let fetcher = creator;
-  return useSWRMutation(
-    shouldFetch ? url : null,
-    fetcher,
-  );
+  return useSWRMutation(shouldFetch ? url : null, fetcher);
 }
 
-export async function fetchCreatedProposals(apiUrl: string, space: string | undefined, author: string | undefined, prefix: string = "") {
+export async function fetchCreatedProposals(
+  apiUrl: string,
+  space: string | undefined,
+  author: string | undefined,
+  prefix: string = "",
+) {
   if (!space || !author) {
     return {
       success: true,
@@ -236,12 +277,13 @@ export async function fetchCreatedProposals(apiUrl: string, space: string | unde
         proposalInfo: {
           snapshotSpace: "",
           proposalIdPrefix: "",
-          minTokenPassingAmount: 0
+          minTokenPassingAmount: 0,
+          nextProposalId: 0,
         },
         proposals: [],
         privateProposals: [],
         hasMore: false,
-      }
+      },
     } as APIResponse<ProposalsPacket>;
   }
 
